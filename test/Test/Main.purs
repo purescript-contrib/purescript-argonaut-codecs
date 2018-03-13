31efb1df44d1e031a5868064dee7d0c17bd88883
Forest Loren Toney III <foresttoney@gmail.com>
2018-03-13 15:40:05 -0700

    Add assocOptional and extendOptional encode combinators

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -3,17 +3,15 @@ module Test.Main where
 import Prelude
 
 import Control.Monad.Eff.Console (log)
-
 import Data.Argonaut.Core (JObject, Json, isObject, toObject)
 import Data.Argonaut.Decode (decodeJson)
-import Data.Argonaut.Encode (encodeJson, (:=), (~>))
+import Data.Argonaut.Encode (class EncodeJson, encodeJson, (:=), (:=?), (~>), (~>?))
 import Data.Argonaut.Gen (genJson)
 import Data.Either (Either(..))
 import Data.Foldable (foldl)
-import Data.Maybe (Maybe(..), maybe, isJust)
+import Data.Maybe (Maybe(..), isJust, isNothing, maybe)
 import Data.StrMap as SM
 import Data.Tuple (Tuple(..))
-
 import Test.StrongCheck (SC, quickCheck, quickCheck', (<?>))
 import Test.StrongCheck.Arbitrary (class Arbitrary)
 import Test.StrongCheck.Gen (suchThat, resize)
@@ -25,6 +23,9 @@ main = do
   combinatorsCheck
 
 newtype TestJson = TestJson Json
+
+instance encodeJsonTestJson :: EncodeJson TestJson where
+  encodeJson (TestJson x) = encodeJson x
 
 instance arbitraryTestJson :: Arbitrary TestJson where
   arbitrary = TestJson <$> (resize 5 genJson)
@@ -59,8 +60,12 @@ combinatorsCheck :: SC () Unit
 combinatorsCheck = do
   log "Check assoc builder `:=`"
   quickCheck' 20 prop_assoc_builder_str
+  log "Check assocOptional builder `:=?`"
+  quickCheck' 20 prop_assoc_optional_builder_str
   log "Check JAssoc append `~>`"
   quickCheck' 20 prop_assoc_append
+  log "Check JAssoc appendOptional `~>?`"
+  quickCheck' 20 prop_assoc_append_optional
   log "Check get field `obj .? 'foo'`"
   quickCheck' 20 prop_get_jobject_field
 
@@ -72,12 +77,26 @@ combinatorsCheck = do
       Tuple k json ->
         (key == k) && (decodeJson json == Right str)
 
+  prop_assoc_optional_builder_str :: Tuple String (Maybe String) -> Boolean
+  prop_assoc_optional_builder_str (Tuple key maybeStr) =
+    case (key :=? maybeStr) of
+      Just (Tuple k json) ->
+        (key == k) && (decodeJson json == Right maybeStr)
+      Nothing -> true
+
   prop_assoc_append :: (Tuple (Tuple String TestJson) Obj) -> Boolean
   prop_assoc_append (Tuple (Tuple key (TestJson val)) (Obj obj)) =
     let appended = (key := val) ~> obj
     in case toObject appended >>= SM.lookup key of
       Just value -> true
       _ -> false
+
+  prop_assoc_append_optional :: Tuple (Tuple String (Maybe TestJson)) Obj -> Boolean
+  prop_assoc_append_optional (Tuple (Tuple key maybeVal) (Obj obj)) =
+    let appended = (key :=? maybeVal) ~>? obj
+    in case toObject appended >>= SM.lookup key of
+      Just value -> isJust maybeVal 
+      _ -> isNothing maybeVal 
 
   prop_get_jobject_field :: Obj -> Boolean
   prop_get_jobject_field (Obj obj) =
