88086c6c5cf2ea699cc3025c4e61c7c3ce716003
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-06-23 16:07:52 +0100

    Add fromJsonString and toJsonString (#109)

--- src/Data/Argonaut/Decode.purs
+++ src/Data/Argonaut/Decode.purs
@@ -1,20 +1,19 @@
 module Data.Argonaut.Decode
-  ( module Data.Argonaut.Decode.Class
+  ( fromJsonString
+  , module Data.Argonaut.Decode.Class
   , module Data.Argonaut.Decode.Combinators
   , module Data.Argonaut.Decode.Error
   , module Data.Argonaut.Decode.Parser
   ) where
 
+import Prelude
+
 import Data.Argonaut.Decode.Class (class DecodeJson, decodeJson)
-import Data.Argonaut.Decode.Combinators
-  ( getField
-  , getFieldOptional
-  , getFieldOptional'
-  , defaultField
-  , (.:)
-  , (.:!)
-  , (.:?)
-  , (.!=)
-  )
+import Data.Argonaut.Decode.Combinators (getField, getFieldOptional, getFieldOptional', defaultField, (.:), (.:!), (.:?), (.!=))
 import Data.Argonaut.Decode.Error (JsonDecodeError(..), printJsonDecodeError)
 import Data.Argonaut.Decode.Parser (parseJson)
+import Data.Either (Either)
+
+-- | Parse and decode a json in one step.
+fromJsonString :: forall json. DecodeJson json => String -> Either JsonDecodeError json
+fromJsonString = parseJson >=> decodeJson
--- src/Data/Argonaut/Encode.purs
+++ src/Data/Argonaut/Encode.purs
@@ -1,16 +1,15 @@
 module Data.Argonaut.Encode
   ( module Data.Argonaut.Encode.Class
   , module Data.Argonaut.Encode.Combinators
+  , toJsonString
   ) where
 
+import Prelude
+
+import Data.Argonaut.Core (stringify)
 import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
-import Data.Argonaut.Encode.Combinators
-  ( assoc
-  , assocOptional
-  , extend
-  , extendOptional
-  , (:=)
-  , (:=?)
-  , (~>)
-  , (~>?)
-  )
+import Data.Argonaut.Encode.Combinators (assoc, assocOptional, extend, extendOptional, (:=), (:=?), (~>), (~>?))
+
+-- | Encode and stringify a type in one step.
+toJsonString :: forall t. EncodeJson t => t -> String
+toJsonString = encodeJson >>> stringify
